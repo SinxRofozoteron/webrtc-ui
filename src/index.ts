c034610c8465e9c8a553d8ce93673f5b27255ce2
unknown <alex.burakou@gmail.com>
2023-07-13 17:01:15 -0600

    Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as AppWrapper } from './AppProvider';
 export { default as Button } from './Button';
+export { default as RoundButton } from './RoundButton';
 export * from './styles';
