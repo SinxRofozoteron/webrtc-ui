6bff02bda49a069a31661308a69c6e8308784531
unknown <alex.burakou@gmail.com>
2023-07-27 07:33:16 -0600

    Added peer deps + react-intl wrapper

--- src/AppWrapper/index.tsx
+++ src/AppWrapper/index.tsx
@@ -1,14 +1,24 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { IntlProvider } from 'react-intl';
 import * as React from 'react';
 
 import { theme } from './theme';
 
+import type { MessageFormatElement } from 'react-intl';
+
 type AppWrapperProps = {
   children: React.ReactNode;
+  messages: Record<string, string> | Record<string, MessageFormatElement[]>;
+  locale: string;
+  defaultLocale?: string;
 };
 
-const AppWrapper = ({ children }: AppWrapperProps) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+const AppWrapper = ({ children, messages, locale, defaultLocale }: AppWrapperProps) => {
+  return (
+    <IntlProvider messages={messages} locale={locale} defaultLocale={defaultLocale}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </IntlProvider>
+  );
 };
 
 export default AppWrapper;
