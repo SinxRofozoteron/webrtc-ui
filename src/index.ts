4db1f9c4c4999f4d4b2598da6ad48fb7fc02ec17
unknown <alex.burakou@gmail.com>
2023-08-09 16:18:00 -0600

    New: Added ImageListItemBar component

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export { default as FormHelperText } from './FormHelperText';
 export { default as Grid } from './Grid';
 export { default as ImageList } from './ImageList';
 export { default as ImageListItem } from './ImageListItem';
+export { default as ImageListItemBar } from './ImageListItemBar';
 export { default as Input } from './Input';
 export { default as InputLabel } from './InputLabel';
 export { default as Menu } from './Menu';
